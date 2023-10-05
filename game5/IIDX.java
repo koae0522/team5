d183cde6b9b3e62037b2afdee38c0a299108dacc
koae0522 <yantukeuma@gmail.com>
2023-10-05 10:16:55 +0900

    Aがキャラ追加

---
+++ game5/IIDX.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class IIDX here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class IIDX extends Actor
+{
+    /**
+     * Act - do whatever the IIDX wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
