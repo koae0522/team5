4a6adb25b680a0f7c7f8f2704627a22cb112ca72
19094hakodatekosen <19094@hakodate.kosen-ac.jp>
2023-11-02 09:56:05 +0900

    キャラクターを表示

---
+++ game5/player1.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class player1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class player1 extends Actor
+{
+    /**
+     * Act - do whatever the player1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public class size extends Actor{
+        private GreenfootImage img_bkup = null;
+        
+        
+    }
+    
+    public void act() 
+    {
+        // Add your action code here.
+        //getWorld().addObject(new player1(),300,100);
+        //getImage().scale(150,100);
+    }    
+}
