0e0ad2d1d11739bbb5c098484ca48dd5abeb3f31
IuyoKatano <yosiuyk@yahoo.co.jp>
2023-11-02 20:37:15 +0900

    Titleの作成 Titleを初期画面になるように設定

---
+++ game5/Title.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Title here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Title extends World
+{
+
+    /**
+     * Constructor for objects of class Title.
+     * 
+     */
+    public Title()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1024, 1024, 1); 
+    }
+    
+    public void act()
+{
+    if(Greenfoot.getKey() != null){
+        World game = new Game();
+        Greenfoot.setWorld( game );
+    }
+}
+    
+    
+    
+    
+}
+
